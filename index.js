ee6ee7a0f98bf777ea28a76ec9a3067879a981c2
unknown <dixi_1903@hotmail.com>
2015-07-12 21:07:19 -0500

    first commint

---
+++ index.js
@@ -0,0 +1,47 @@
+var dblocation = "https://docs.google.com/spreadsheets/d/1F-T6Z519mYgB8XxmH-oWmXmpiRT1R9N-61XlexvTACc/edit?usp=sharing";
+    
+function fillChart(response) {
+    "use strict";
+    
+    var data, options, chart;
+    
+    data = response.getDataTable();
+    
+     options = {
+        title: "Espectro",
+        hAxis: {title: "Economico", 
+                maxValue: 10,
+                minValue: -10,
+                minorGridlines: {count: 4}
+                },
+        vAxis: {title: "Libertades personales",
+                maxValue: 10,
+                minValue: -10,
+                minorGridlines: {count: 4}
+               },
+        bubble: {textStyle: {fontSize: 11},
+                 maxSize: 2
+                }
+    };
+   
+
+    chart = new google.visualization.BubbleChart(document.getElementById("espectrochart"));
+    
+    chart.draw(data, options)
+}
+
+function readData() {
+    "use strict";
+    var query;
+    
+    
+    query = new google.visualization.Query(dblocation);
+    query.setQuery("select *");
+    query.send(fillChart);
+}
+
+$(document).ready(function () { 
+    "use strict";
+    
+    readData();
+});
